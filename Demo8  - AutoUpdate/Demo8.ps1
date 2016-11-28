577c4d86ffb98ffec461645f97cc22f59ccb29d5
Bartek Bielawski <bartek.bielawski@live.com>
2016-11-28 21:42:28 +0100

    Last but not least - Demo8

---
+++ Demo8  - AutoUpdate/Demo8.ps1
@@ -0,0 +1,7 @@
+Import-Module -FullyQualifiedName @{ ModuleName = 'ExpertsModule'; RequiredVersion = '1.0.0.0' }
+Get-Uptime
+Find-Module -Repository Artifactory -Name ExpertsModule
+New-Password
+Get-Module -Name ExpertsModule
+Get-Module -Name ExpertsModule -ListAvailable
+psedit C:\Git\Demo8\ExpertsModule\ExpertsModule.psm1
